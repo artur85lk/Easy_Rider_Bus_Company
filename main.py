ed344b08dba34a1aaab2792e174caed22d48aa0e
artur85lk <artur85lk@o2.pl>
2022-10-27 13:10:48 +0200

    4/6 new

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-import json
 
+
+import json
+import re
 
 date = input()
+# date = json.dumps(date)  # do testów
+
 y = json.loads(date)
 
+line = {}
+good = True
 
-bus_stops = []
+for i in y:
+    line[i["bus_id"]] = ""
+for i in y:
+    line[i["bus_id"]] += i["stop_type"]
+for k, v in line.items():
+    if not bool(re.match(r'S.?.?.?.?F', v)):
+        print(f"There is no start or end stop for the line: {k}.")
+        good = False
+# transfer
+transfer = []
+for i in y:
+    more_two = 0
+    for j in y:
+        if j["stop_name"] == i["stop_name"]:
+            more_two += 1
+    if more_two > 1:
+        transfer.append(i["stop_name"])
+    more_two = 0
 
-for i in range(0, len(y)):
-    bus_stops.append(y[i]['bus_id'])
-
-print(bus_stops)
-bus_s_set = set(bus_stops)
-print("Line names and number of stops:")
-for i in bus_s_set:
-    print(f"bus_id: {i}, stops: {bus_stops.count(i)}")
-
-
+start = sorted(list(set([i["stop_name"] for i in y if i["stop_type"] == "S"])))
+transfer = sorted(list(set(transfer)))
+# transfer = [i["stop_name"] for i in y if not re.match(r'[FS]', i["stop_type"])]
+finish = sorted(list(set([i["stop_name"] for i in y if i["stop_type"] == "F"])))
+if good:
+    print(f"Start stops: {len(start)} {start}")
+    print(f"Transfer stops: {len(transfer)} {transfer}")
+    print(f"Finish stops: {len(finish)} {finish}")
