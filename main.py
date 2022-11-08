30282caf4cd09211e17910e4a4c68738ea888f31
artur85lk <artur85lk@o2.pl>
2022-11-08 22:46:20 +0100

    6/6 new

--- main.py
+++ main.py
@@ -1,50 +1,28 @@
 import json
 
-
 date = input()
-# date = [
-#     {
-#         "bus_id": 512,
-#         "stop_id": 4,
-#         "stop_name": "Bourbon Street",
-#         "next_stop": 6,
-#         "stop_type": "S",
-#         "a_time": "08:13"
-#     },
-#     {
-#         "bus_id": 512,
-#         "stop_id": 6,
-#         "stop_name": "Sunset Boulevard",
-#         "next_stop": 0,
-#         "stop_type": "F",
-#         "a_time": "08:16"
-#     }
-# ]
-# date = json.dumps(date)  # do testów
 
 y = json.loads(date)
-step = []
 
+key_bus_id = {}
 for i in y:
-    step.append([i["stop_name"], i["a_time"], i["bus_id"]])
-last_value = 0
-counter = 0
-good = True
-rong_step = True
-print(f"Arrival time test:")
-for s, v, k in step:
-    new_v = [int(v[0:2] + v[3:5]), k]
-    if counter == 0:
-        last_value = new_v
-    if counter > 0 and last_value[0] > new_v[0] and new_v[1] == last_value[1] and rong_step:
-        print(f"bus_id line {k}: wrong time on station {s}")
-        good = False
-        rong_step = False
-    if new_v[1] != last_value[1]:
-        rong_step = True
-    last_value = new_v
-    counter += 1
-if good:
+    if i["stop_type"] == "":
+        i["stop_type"] = "J"
+    try:
+        if key_bus_id[i["stop_name"]]:
+            key_bus_id[i["stop_name"]] = key_bus_id[i["stop_name"]] + i["stop_type"]
+    except:
+        key_bus_id[i["stop_name"]] = i["stop_type"]
+finish = []
+
+for k, v in key_bus_id.items():
+    if len(v) > 1 and v[0] != v[1]:
+        if v[0] == "O" or v[1] == "O":
+            finish.append(k)
+print("On demand stops test:")
+result = set(finish)
+result = sorted(list(result), reverse=False)
+if len(result):
+    print(f"Wrong stop type: {result}")
+else:
     print("OK")
-
-
